2dc8805151c53a4970d1c2c7486dbd58ece07ed6
mutia109 <mutiaraplg00@gmail.com>
2022-04-02 16:08:08 +0700

    form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,12 @@
-const SearchBar = () => {
+import React from 'react'
+const SearchBar = ({onSubmit, handleInput}) => { 
     return (
-      <form className="formSearch">
-        <div className="form-group">
-          <input type="text" name="query" placeholder="Search gif..." required />
-          <button type='submit'> Search</button>
-        </div>
-      </form>
+      <form className="formSearch" onSubmit={onSubmit}>
+                <div className="form-group">
+                <input type="text" name="query" placeholder="Search gif..." required onChange={handleInput}/>
+                <button type='submit' id='btn'> Search</button>
+                </div>
+            </form>
     );
   };
-  
   export default SearchBar;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,15 +3,7 @@ import './index.css';
 import Gif from '../../components/Gif';
 import data from '../../data/dataGif';
 import { useState } from 'react';
-
-// const gif = {
-//     id: '4HrBfVJJveBNS9ytSk',
-//     title: 'Nintendo Plotting GIF by Gaming GIFs',
-//     uploadedDate: '2018-04-03 15:21:50',
-//     url: 'https://media4.giphy.com/media/4HrBfVJJveBNS9ytSk/200w.gif?cid=cb3f2bebpuo6jj0g5f9gfibjre2zzbb4yb1cfshtplanlrpw&rid=200w.gif&ct=g',
-//     webp: 'https://media4.giphy.com/media/4HrBfVJJveBNS9ytSk/giphy.webp?cid=cb3f2bebpuo6jj0g5f9gfibjre2zzbb4yb1cfshtplanlrpw&rid=giphy.webp&ct=g',
-//   }
-
+import SearchBar from '../../components/SearchBar';
 
 function Home() {
     const [text, setText] = useState('');
@@ -32,12 +24,7 @@ function Home() {
     
     return (
         <div className="home">
-            <form className="formSearch" onSubmit={onSubmit}>
-                <div className="form-group">
-                <input type="text" name="query" placeholder="Search gif..." required onChange={handleInput}/>
-                <button type='submit' id='btn'> Search</button>
-                </div>
-            </form>
+            <SearchBar onSubmit = {onSubmit} handleInput={handleInput}/>
             <div className='gifs'>
                 {gifs.map(gif => (
                     <React.Fragment key ={gif.id}> 
